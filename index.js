b516bbcf15323d3d7a6826ec054b48dc8de0be00
mohadese azizi <mohadeseazizi2018@gmail.com>
2021-10-19 16:04:38 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("text");
+function slideValue(val) {
+    output.innerText = val;
+  }
+   
+  var slider = document.getElementById("myRange");
+var output = document.getElementById("text");
+function slideValue(val) {
+    output.innerText = val;
+  }
